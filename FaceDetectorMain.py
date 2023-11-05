560c06658f9f844d135450625214a85af3bcc90d
rrsr28 <rr.sanjayram@gmail.com>
2023-11-05 10:13:42 +0530

    Test code

--- FaceDetectorMain.py
+++ FaceDetectorMain.py
@@ -7,53 +7,41 @@ def detect_faces(image):
     # Convert the image to grayscale
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    # Detect faces in the image
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
-
-    # Draw bounding boxes around the detected faces
     for (x, y, w, h) in faces:
         cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
     return image
 
 def detect_faces_in_image(image_path):
-    # Load the image
-    image = cv2.imread(image_path)
 
-    # Detect faces in the image
+    image = cv2.imread(image_path)
     result = detect_faces(image)
 
-    # Display the result
     cv2.imshow('Face Detection', result)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
 def detect_faces_in_video(video_path):
-    # Open the video file
+    
     video = cv2.VideoCapture(video_path)
 
     while True:
-        # Read a frame from the video
         ret, frame = video.read()
 
         if not ret:
             break
-
-        # Detect faces in the frame
+        
         result = detect_faces(frame)
-
-        # Display the result
         cv2.imshow('Face Detection', result)
 
-        # Exit if the 'q' key is pressed
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
 
-    # Release the video file and close the windows
     video.release()
     cv2.destroyAllWindows()
 
-image_path = 'Images/Crowd1.jpg'
+image_path = 'Images/Crowd1.jpeg'
 detect_faces_in_image(image_path)
 
 #video_path = 'path/to/video.mp4'
