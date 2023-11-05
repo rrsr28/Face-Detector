0f400a94de9cb5cd4284354d6fb19c0bc228e621
rrsr28 <rr.sanjayram@gmail.com>
2023-11-05 10:28:53 +0530

    Added UI and some more changes

--- FaceDetectorMain.py
+++ FaceDetectorMain.py
@@ -5,38 +5,33 @@ import numpy as np
 
 app = Flask(__name__)
 
-def detect_faces(image):
-
-    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
-
-    for (x, y, w, h) in faces:
-        cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
-
-    return image
-
 @app.route('/')
 def index():
     return render_template('index.html')
 
 @app.route('/detect', methods=['POST'])
-def detect():
+def detect_faces():
 
     image_data = request.form['imageData']
-    image_data = base64.b64decode(image_data)
 
-    image_array = np.frombuffer(image_data, np.uint8)
+    # Remove the base64 encoding and convert to numpy array
+    image_data = base64.b64decode(image_data.split(',')[1])
+    nparr = np.frombuffer(image_data, np.uint8)
+    image = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
-    image = cv2.imdecode(image_array, cv2.IMREAD_COLOR)
-    result = detect_faces(image)
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    # Encode the result image as base64
-    _, result_data = cv2.imencode('.jpg', result)
-    result_base64 = base64.b64encode(result_data).decode('utf-8')
+    face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+    faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
 
-    return jsonify(result=result_base64)
+    for (x, y, w, h) in faces:
+        cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
+
+    # Convert the result image to base64
+    _, result_image = cv2.imencode('.jpg', image)
+    result_image_base64 = base64.b64encode(result_image).decode('utf-8')
+
+    return jsonify(result=result_image_base64)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
