bfe64bd82d36ded66aae9d7850e767ee0556fc72
rrsr28 <rr.sanjayram@gmail.com>
2023-11-05 16:33:08 +0530

    How the tables have turned

--- FaceDetectorMain.py
+++ FaceDetectorMain.py
@@ -1,48 +1,45 @@
-from flask import Flask, render_template, request
 import cv2
-import os
-from PIL import Image
+import tkinter as tk
+from tkinter import filedialog
+from PIL import ImageTk, Image
 
-app = Flask(__name__)
+def detect_faces(image_path):
 
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-@app.route('/detect', methods=['POST'])
-def detect():
-    # Get the uploaded image file
-    image_file = request.files['image']
-
-    # Save the image file to a temporary location
-    image_path = 'temp.jpg'
-    image_file.save(image_path)
-
-    # Load the pre-trained face detection model
     face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
-    # Read the image
     image = cv2.imread(image_path)
-
-    # Convert the image to grayscale
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-
-    # Detect faces in the image
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
 
-    # Draw bounding boxes around the detected faces
     for (x, y, w, h) in faces:
         cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-    # Save the output image with the bounding boxes
-    output_filename = 'output.jpg'
-    output_path = os.path.join(app.static_folder, output_filename)
-    cv2.imwrite(output_path, image)
+    cv2.imshow('Faces Detected', image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 
-    # Remove the temporary image file
-    os.remove(image_path)
+def select_image():
 
-    return render_template('result.html', image_path=output_path)
+    image_path = filedialog.askopenfilename(initialdir='/', title='Select Image', filetypes=(('Image Files', '*.jpg *.jpeg *.png'), ('All Files', '*.*')))
+    image = Image.open(image_path)
+    image = image.resize((400, 400), Image.ANTIALIAS)
+    
+    # Convert the image to Tkinter format
+    image_tk = ImageTk.PhotoImage(image)
+    
+    # Update the image preview in the UI
+    image_preview.config(image=image_tk)
+    image_preview.image = image_tk
+    
+    detect_faces(image_path)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+window = tk.Tk()
+window.title('Face Detection App')
+
+select_button = tk.Button(window, text='Select Image', command=select_image)
+select_button.pack(pady=10)
+
+image_preview = tk.Label(window)
+image_preview.pack()
+
+window.mainloop()
