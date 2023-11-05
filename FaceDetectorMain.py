c6aa70c33d9106e68c514b7acb2984623971aa2b
rrsr28 <rr.sanjayram@gmail.com>
2023-11-05 16:36:37 +0530

    Added no.of faces recongd feature

--- FaceDetectorMain.py
+++ FaceDetectorMain.py
@@ -1,16 +1,15 @@
 import cv2
 import tkinter as tk
 from tkinter import filedialog
-from PIL import ImageTk, Image
 
 def detect_faces(image_path):
-
     face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
     image = cv2.imread(image_path)
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
 
+    num_faces = len(faces)
     for (x, y, w, h) in faces:
         cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
@@ -18,19 +17,10 @@ def detect_faces(image_path):
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
-def select_image():
+    num_faces_label.config(text=f"Number of faces recognized: {num_faces}")
 
+def select_image():
     image_path = filedialog.askopenfilename(initialdir='/', title='Select Image', filetypes=(('Image Files', '*.jpg *.jpeg *.png'), ('All Files', '*.*')))
-    image = Image.open(image_path)
-    image = image.resize((400, 400), Image.ANTIALIAS)
-    
-    # Convert the image to Tkinter format
-    image_tk = ImageTk.PhotoImage(image)
-    
-    # Update the image preview in the UI
-    image_preview.config(image=image_tk)
-    image_preview.image = image_tk
-    
     detect_faces(image_path)
 
 window = tk.Tk()
@@ -39,7 +29,7 @@ window.title('Face Detection App')
 select_button = tk.Button(window, text='Select Image', command=select_image)
 select_button.pack(pady=10)
 
-image_preview = tk.Label(window)
-image_preview.pack()
+num_faces_label = tk.Label(window, text="Number of faces recognized: ")
+num_faces_label.pack()
 
 window.mainloop()
